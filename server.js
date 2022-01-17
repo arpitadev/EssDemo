fbc798b4fda1951c647f72f36abe77633328228c
Arpita Devargaonkar <ArpitaD@WinjitTechnologiesPvtLtd.local>
2022-01-17 23:02:15 +0530

    Deploy to Heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/dashboard-app'));
+app.use(express.static('./dist/ESSTimesheet'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/dashboard-app/'}),
+    res.sendFile('index.html', {root: 'dist/ESSTimesheet/'}),
 );
 
 // Start the app by listening on the default Heroku port
